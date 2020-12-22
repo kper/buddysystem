a11f945e7a7001b35aa175a9477e64b83f13686e
Kevin Per <kevin.per@protonmail.com>
2020-12-22 20:57:19 +0100

    Fix angular production settings

--- frontend/src/app/global/globals.ts
+++ frontend/src/app/global/globals.ts
@@ -1,8 +1,9 @@
 import {Injectable} from '@angular/core';
+import { environment } from './../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class Globals {
-  readonly backendUri: string = 'http://localhost:8080/api/v1';
+  readonly backendUri: string = environment.apiUrl;
 }
