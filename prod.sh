d844b9eec5a37907ad0b1ffdc3498b09995fb526
Kevin Per <kevin.per@protonmail.com>
2020-12-23 13:14:08 +0100

    Add mvn to `prod.sh`

--- prod.sh
+++ prod.sh
@@ -8,4 +8,6 @@ cd frontend && ng build --prod
 echo "Copy"
 mkdir ./../backend/src/main/resources/static/
 cp -r dist/buddysystem/* ./../backend/src/main/resources/static/
-	
+
+echo "Build jar"
+cd ../backend && mvn clean compile package
