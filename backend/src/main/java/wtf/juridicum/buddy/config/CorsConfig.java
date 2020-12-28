11fcefb69eeed52e1ba52b87f47b8902d9a661b1
Kevin Per <kevin.per@protonmail.com>
2020-12-28 12:58:39 +0100

    Fixing configuration in cors

--- backend/src/main/java/wtf/juridicum/buddy/config/CorsConfig.java
+++ backend/src/main/java/wtf/juridicum/buddy/config/CorsConfig.java
@@ -12,6 +12,6 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS").allowedOrigins("*");
+                .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS").allowedOriginPatterns("*");
     }
 }
